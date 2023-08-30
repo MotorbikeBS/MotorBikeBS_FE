aa03a01dcd764edce9418c709d73c66fb09468c7
NhutMinhIT <nhutminh.it19@gmail.com>
2023-08-30 23:19:11 +0700

    Divide layout Home Page of Admin and Buyer

---
+++ src/pages/buyer/home-component/HomeComponent.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import AdminMenu from "../../../common-components/menu-component/AdminMenu";
+import Footer from "../../../common-components/footer-component/Footer";
+
+
+const CustomerHome = () => {
+    return (
+
+        <Box display="flex" flexDirection="column" height="100vh" width='100%'>
+            <AdminMenu />
+            <Box flexGrow={2}>
+
+                <Typography>
+                    Đây là Slide
+                </Typography>
+
+            </Box>
+            <Box flexGrow={8} sx={{ background: 'yellow' }}>
+                <Typography variant="h5">Admin Home</Typography>
+            </Box>
+
+            <Box flexGrow={2} sx={{ background: 'red' }}>
+                <Footer />
+            </Box>
+        </Box>
+
+    );
+};
+
+export default CustomerHome;
