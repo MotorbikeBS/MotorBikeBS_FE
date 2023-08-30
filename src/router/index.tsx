42806aab4fae244fe3ad0a74cf9ea9fa1c5ae2d3
NhutMinhIT <nhutminh.it19@gmail.com>
2023-08-30 17:30:55 +0700

    Done UI Forgot Password

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Register from '../pages/register-components/Register'
 import LoginComponent from '../pages/login-component/LoginComponent'
+import ForgotPasswordComponent from '../pages/forgot-password-component/ForgotPasswordComponent'
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="*" element={<LoginComponent />} />
 
             <Route path='/sign-up' element={<Register />} />
+            <Route path='/login' element={<LoginComponent />} />
+            <Route path='/forgot-password' element={<ForgotPasswordComponent />} />
         </Routes>
     )
 }
