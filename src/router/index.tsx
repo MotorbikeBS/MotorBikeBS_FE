bb9c36d284c4c4b82e5123f0978e63fc3bb05d75
Minh Trí <phanminhtri269@gmail.com>
2023-08-30 22:10:53 +0700

    create admin menu

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Route, Routes, Navigate } from 'react-router-dom'
-import Register from '../pages/register-components/Register'
-import LoginComponent from '../pages/login-component/LoginComponent'
-import ForgotPasswordComponent from '../pages/forgot-password-component/ForgotPasswordComponent'
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Register from "../pages/register-components/Register";
+import LoginComponent from "../pages/login-component/LoginComponent";
+import ForgotPasswordComponent from "../pages/forgot-password-component/ForgotPasswordComponent";
+import AdminHome from "../pages/home/AdminHome";
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="*" element={<LoginComponent />} />
+  return (
+    <Routes>
+      <Route path="*" element={<LoginComponent />} />
 
-            <Route path='/sign-up' element={<Register />} />
-            <Route path='/login' element={<LoginComponent />} />
-            <Route path='/forgot-password' element={<ForgotPasswordComponent />} />
-        </Routes>
-    )
-}
+      <Route path="/sign-up" element={<Register />} />
+      <Route path="/login" element={<LoginComponent />} />
+      <Route path="/forgot-password" element={<ForgotPasswordComponent />} />
 
-export default AppRoutes
+      <Route path="/admin-home" element={<AdminHome />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
