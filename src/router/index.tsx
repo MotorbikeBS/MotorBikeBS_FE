b8462d8094ede88ac815a2679b46270b93b57182
NhutMinhIT <nhutminh.it19@gmail.com>
2023-08-31 01:03:41 +0700

    router comment

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Register from "../pages/register-components/Register";
 import LoginComponent from "../pages/login-component/LoginComponent";
 import ForgotPasswordComponent from "../pages/forgot-password-component/ForgotPasswordComponent";
@@ -8,16 +8,23 @@ import CustomerHome from "../pages/buyer/home-component/HomeComponent";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="*" element={<LoginComponent />} />
-
+      {/* Common Router  */}
       <Route path="/sign-up" element={<Register />} />
       <Route path="/login" element={<LoginComponent />} />
       <Route path="/forgot-password" element={<ForgotPasswordComponent />} />
-      //Admin Router
+
+      {/* Page Not Found  */}
+      <Route path="*" element={<LoginComponent />} />
+
+      {/* Admin Router */}
       <Route path="/admin-home" element={<AdminHome />} />
 
-      //BuyerRouter
+      {/* BuyerRouter */}
       <Route path="/customer-home" element={<CustomerHome />} />
+
+      {/* Owner Router  */}
+
+      {/* Store Owner */}
 
     </Routes>
   );
