71f7cda1f00d90be2533fd26de7d24c59241feed
NhutMinhIT <nhutminh.it19@gmail.com>
2023-09-01 02:39:54 +0700

    fix import and dependency

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Typography } from '@mui/material'
 import './App.css';
-import LoginComponent from '../pages/login-component/LoginComponent';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from '../router';
 
