27a11947a870bbcb9b57fbeb2e4f0cc9a4d772b3
NhutMinhIT <nhutminh.it19@gmail.com>
2023-09-03 18:42:20 +0700

    update menu v2

--- src/pages/page-notfound-component/PageNotFound.tsx
+++ src/pages/page-notfound-component/PageNotFound.tsx
@@ -1,7 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import * as React from 'react';
 import './style/style.scss';
-import MenuComponent from '../../common-components/menu-component/MenuComponent';
 
 const PageNotFound = () => {
     return (
