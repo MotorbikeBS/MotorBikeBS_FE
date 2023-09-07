34e68124acbd9b9804c722efc600f67d3ea850cf
Minh Trí <phanminhtri269@gmail.com>
2023-09-07 14:37:20 +0700

    Update UI store list

--- src/pages/customer/store-list/StoreList.tsx
+++ src/pages/customer/store-list/StoreList.tsx
@@ -1,99 +1,17 @@
 import React from "react";
-import { IStore } from "./model/Store";
-import { Box, Button, Typography } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { Item } from "./style/style-root";
-import CustomerMenuComponent from "../customer-menu-component/CustomerMenuComponent";
-import "./style/style.scss";
+import { Box } from "@mui/material";
 import FooterComponent from "../../../common-components/footer-component/FooterComponent";
-
-const items: IStore[] = [
-  {
-    id: 1,
-    image:
-      "https://lh3.googleusercontent.com/p/AF1QipMBxbOVy3RITmfwsro460qSkKPAT_mCaJmFUeTZ=s1360-w1360-h1020",
-    store_name: "Vũ Phụng Hoàng",
-    store_phone: "0909170111",
-    address: "Quận 8, Thành phố Hồ Chí Minh",
-    store_email: "vuphuonghoangxe@gmail.com"
-  },
-  {
-    id: 2,
-    image:
-      "https://lh3.googleusercontent.com/p/AF1QipMBxbOVy3RITmfwsro460qSkKPAT_mCaJmFUeTZ=s1360-w1360-h1020",
-    store_name: "Vũ Phụng Hoàng",
-    store_phone: "0909170111",
-    address: "Quận 8, Thành phố Hồ Chí Minh",
-    store_email: "vuphuonghoangxe@gmail.com"
-  },
-  {
-    id: 3,
-    image:
-      "https://lh3.googleusercontent.com/p/AF1QipMBxbOVy3RITmfwsro460qSkKPAT_mCaJmFUeTZ=s1360-w1360-h1020",
-    store_name: "Vũ Phụng Hoàng",
-    store_phone: "0909170111",
-    address: "Quận 8, Thành phố Hồ Chí Minh",
-    store_email: "vuphuonghoangxe@gmail.com"
-  },
-  {
-    id: 4,
-    image:
-      "https://lh3.googleusercontent.com/p/AF1QipMBxbOVy3RITmfwsro460qSkKPAT_mCaJmFUeTZ=s1360-w1360-h1020",
-    store_name: "Vũ Phụng Hoàng",
-    store_phone: "0909170111",
-    address: "Quận 8, Thành phố Hồ Chí Minh",
-    store_email: "vuphuonghoangxe@gmail.com"
-  }
-];
+import StoreListComponent from "./StoreListComponent";
+import CustomerMenuComponent from "../customer-menu-component/CustomerMenuComponent";
+import BannerComponent from "../../../common-components/banner-component/BannerComponent";
 
 const StoreList = () => {
   return (
     <>
       <CustomerMenuComponent />
+      <BannerComponent />
 
-      <Box sx={{ flexGrow: 1, margin: "0 48px 0 48px" }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 1 }}
-          columns={{ xs: 4, sm: 6, md: 12 }}
-          className="store-grid"
-        >
-          {items.map((item) => (
-            <Grid xs={2} sm={8} md={3} key={item.id}>
-              <Item className="store-item">
-                <div className="store-image">
-                  <img src={item.image} alt="Đây là ảnh sản phẩm" />
-                </div>
-                <div className="store-information">
-                  <Typography variant="h6">
-                    <strong>Cửa hàng: </strong>
-                    {item.store_name}
-                  </Typography>
-                  <div className="store-info-content">
-                    <Typography>
-                      <strong>Điện thoại: </strong>
-                      {item.store_phone}
-                    </Typography>
-                    <Typography>
-                      <strong>Địa chỉ: </strong>
-                      {item.address}
-                    </Typography>
-                    <Typography>
-                      <strong>Email: </strong>
-                      {item.store_email}
-                    </Typography>
-                  </div>
-                </div>
-
-                <div className="btn-style">
-                  <Button variant="outlined">Chi tiết</Button>
-
-                </div>
-              </Item>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <StoreListComponent />
 
       <Box flexGrow={1} className="footer-style">
         <FooterComponent />
