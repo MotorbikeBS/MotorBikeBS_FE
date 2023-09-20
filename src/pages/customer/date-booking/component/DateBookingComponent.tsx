47607999fdc2f67f2c448cd0419ba81c62b7cf3a
Minh Trí <phanminhtri269@gmail.com>
2023-09-20 12:19:56 +0700

    edit table date-booking

--- src/pages/customer/date-booking/component/DateBookingComponent.tsx
+++ src/pages/customer/date-booking/component/DateBookingComponent.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { rows, columns } from './table/table';
-import '../style/style.scss'
 
 const DateBookingComponent = () => {
     return (
         <>
             <Box m={6}>
-                {/* <Typography variant="h5" className='title_date_booking'>Lịch hẹn</Typography> */}
                 <DataGrid
                     rows={rows}
                     columns={columns}
